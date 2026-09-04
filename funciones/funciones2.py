8a9a5d7bf5262e954119b3082a54739c82f15b0a
José Durán <jose.duran@uamv.edu.ni>
2026-09-04 08:43:43 -0600

    Update funciones2

--- funciones/funciones2.py
+++ funciones/funciones2.py
@@ -14,3 +14,16 @@ resta = restar(numero2= 6, numero1 = 2)
 print(resta)
 resta = restar(8)
 print(resta)
+
+
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+def dividir(numero1, numero2):
+    try:
+        return numero1 / numero2
+    except ZeroDivisionError:
+        return "El segundo valor deb ser mayor que 0"
+
+print(multiplicar(4, 2))
+print(dividir(20, 5))
