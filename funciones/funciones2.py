e0e73c8bf0b0be9de0ceb5059e2cf2226f3710ba
José Durán <jose.duran@uamv.edu.ni>
2026-09-04 08:21:41 -0600

    update funciones2

--- funciones/funciones2.py
+++ funciones/funciones2.py
@@ -4,3 +4,13 @@ def sumar(numero1, numero2):
 
 suma = sumar(15, 17)
 print(suma)
+
+def restar(numero1 = 0, numero2 = 2):
+    return numero1 - numero2
+
+resta = restar(4, 2)
+print(resta)
+resta = restar(numero2= 6, numero1 = 2)
+print(resta)
+resta = restar(8)
+print(resta)
